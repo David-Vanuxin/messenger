29e5e898385a31f462e15c5bf4845499d8757fd0
David-Vanuxin <2468steed@gmai.com>
2025-06-27 20:34:57 +0300

    Проект создан

---
+++ messenger.js
@@ -0,0 +1,54 @@
+const http = require('node:http')
+const { spawn } = require('node:child_process');
+const { readFileSync, readdirSync } = require('node:fs')
+const { createInterface } = require("node:readline")
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+let message = ""
+
+rl.on("line", line => {
+  message = line
+})
+
+rl.on("SIGINT", () => process.exit(0))
+
+const indexPageContent = readFileSync(__dirname + '/index.html').toString()
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    res.end(indexPageContent)
+    return
+  }
+
+  const url = new URL(req.url, `http://${req.headers.host}`)
+  const text = url.searchParams.get("text")
+  if (text) console.log("\n" + text)
+
+  req.on("data", data => {
+    if (!data) return
+    const body = new URLSearchParams(data.toString())
+    console.log()
+    console.log(body.get("text"))
+  })
+
+  const sended = message
+  message = ""
+
+  res.end(sended)
+});
+
+server.listen(8000, () => {
+  const hostname = spawn("hostname", ['-I'])
+
+  hostname.stdout.on("data", data => {
+    if (data.toString().length === 1) data = "127.0.0.1"
+    const path = `http://${data.toString().trim()}:8000\n`
+    console.log(path)
+    const qr = spawn("qr", [path], {stdio: 'inherit'})
+  })
+
+});
